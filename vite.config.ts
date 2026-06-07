a07ea185320dab24266fe6ffe2429629db2aafc8
yuni susanti <waratzain@gmail.com>
2026-06-08 00:07:29 +0800

    Upload hasil build terbaru

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,24 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    
     return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
+        // Penting untuk GitHub Pages agar file ditemukan
+        base: '/technobeta-05/', 
+        
+        server: {
+            port: 3000,
+            host: '0.0.0.0',
+        },
+        plugins: [react()],
+        define: {
+            'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, '.'),
+            }
         }
-      }
     };
 });
